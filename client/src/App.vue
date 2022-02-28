f45103b1e4fdf3fd9623337c7cfe1e44a9771d53
NaveenaKota <kotan1@newpaltz.edu>
2022-02-28 15:11:47 -0500

    Working as good as noframework

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
+import Nav from './components/Nav.vue';
+import Messages from './pages/Messages.vue';
     
-</script> 
+
+</script>
 
 <template>
-   <Nav />
-   <h1 class="title">
-     Praying Ukraine
-   </h1>
+    <Nav />
+    <div class = "container">
+      <section class="section">
+         <h1 class="title">
+            Praying Ukraine
+         </h1>
+      </section>
+    </div>
+    
+    <Messages />
+    
 </template>
 
 <style>
