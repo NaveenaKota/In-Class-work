857269c4fd01707bd49b87fb4e2578876617c865
NaveenaKota <kotan1@newpaltz.edu>
2022-03-07 14:10:39 -0500

    Proper routing and linking

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import 'bulma';
 import '@fortawesome/fontawesome-free/css/all.css';
+import './assets/style/main.scss';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
